9ab804abb1c83704d775061341b04029b67eccba
nevermind322 <alexfilonov2015@yandex.ru>
2023-11-30 21:33:47 +0300

    cleanup

--- app/src/main/java/com/example/testovoe/api/EasypayService.kt
+++ app/src/main/java/com/example/testovoe/api/EasypayService.kt
@@ -6,6 +6,7 @@ import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Headers
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 const val appKey= "12345"
 const val version = "1"
